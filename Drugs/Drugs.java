b5e1f81334316981d54466439809fd107bc1e9c4
ngomanhhieu <139697488+ngomanhhieu@users.noreply.github.com>
2024-12-04 21:16:49 +0700

    .

--- Drugs/Drugs.java
+++ Drugs/Drugs.java
@@ -12,8 +12,10 @@ public class Drugs {
 
     // Thêm thuốc vào Drugs
     private void addPotions() {
-        potions.add(new Potion("Health Potion", 50, "Restore 50 HP", 50));
-        potions.add(new Potion("Mana Potion", 70, "Restore 30 MP", 30));
+        // Thêm thuốc hồi máu
+        potions.add(new Potion("Health Potion", 50, "Restore 50 HP", 50, 5));
+        // Thêm thuốc hồi mana
+        potions.add(new Potion("Mana Potion", 70, "Restore 30 MP", 30, 3));
     }
 
     // Phương thức lấy tất cả các thuốc
@@ -28,5 +30,43 @@ public class Drugs {
             System.out.println(potion);
         }
     }
-}
 
+    // Sử dụng thuốc
+    public String usePotion(String potionName) {
+        for (Potion potion : potions) {
+            if (potion.getName().equalsIgnoreCase(potionName)) {
+                return potion.usingItems();
+            }
+        }
+        return "Potion not found.";
+    }
+
+    // Lớp con đại diện cho Potion
+    public static class Potion extends Items {
+        private int duration; // Thời gian tác dụng (giây)
+
+        public Potion(String name, int price, String effect, int attributePoints, int duration) {
+            super(name, "Drug", price, effect, attributePoints);
+            this.duration = duration;
+        }
+
+        public int getDuration() {
+            return duration;
+        }
+
+        public void setDuration(int duration) {
+            this.duration = duration;
+        }
+
+        @Override
+        public String usingItems() {
+            return "You used " + getName() + ". Effect: " + getEffect() + " for " + duration + " seconds.";
+        }
+
+        @Override
+        public String toString() {
+            return "Potion{name='" + getName() + "', effect='" + getEffect() +
+                    "', attributePoints=" + getAttributePoints() + ", duration=" + duration + " seconds}";
+        }
+    }
+}
